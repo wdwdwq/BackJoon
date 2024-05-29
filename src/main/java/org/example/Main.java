1fc1f75bead72aede01fb9ee9b582ff99369ca58
wdwdwq <qkenr7278@gmail.com>
2024-05-29 12:08:41 +0900

    BackJoon2840(2)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,48 +11,19 @@ public class Main {
         b = in.nextInt();
         c = in.nextInt();
 
-        // 만약 모든 변수가 다른 경우
-        if (a != b && b != c && a != c) {
-            int max;
-            // 만약 a > b 라면
-            if (a > b) {
-                // c > a > b 라면
-                if (c > a) {
-                    max = c;
-                }
-                // a > (b, c)
-                else {
-                    max = a;
-                }
-            }
-            // b > a 라면
-            else {
-                // c > b > a 라면
-                if (c > b) {
-                    max = c;
-                }
-                // b > (a, c)
-                else {
-                    max = b;
-                }
-            }
-            System.out.println(max * 100);
+        if(a == b &&  b == c){ //모든 변수가 다른 경우
+            System.out.println(10000 + a * 1000);
         }
-        // 적어도 한 쌍 이상의 서로 같은 변수가 존재할 경우
-        else {
-            // 3개의 변수가 모두 같은 경우
-            if (a == b && a == c) {
-                System.out.println(10000 + a * 1000);
-            } else {
-                // a가 b혹은 c랑만 같은 경우
-                if (a == b || a == c) {
-                    System.out.println(1000 + a * 100);
-                }
-                // b가 c랑 같은 경우
-                else {
-                    System.out.println(1000 + b * 100);
-                }
+        else if( a == b || b == c || a == c){ // 두 개 변수가 같은경우
+            if(a == b || a == c){ // a가 b 또는 c와 같은 경우
+                System.out.println(1000 + a * 100);
+            }else{//b가 c와 같은 경우
+                System.out.println(1000 + b * 100);
             }
+        }
+        else{ //모든 변수가 다른 경우
+            int max = Math.max(a,Math.max(b, c));
+            System.out.println(max * 100);
         }
     }
 }
@@ -62,4 +33,6 @@ public class Main {
 //3. 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
 
 //3개의 변수가 모두 다른, 아닌지 판단
-//
+//1.모든 변수가 같은 경우 10000 + a * 1000이 출력 (1,2,3 = 11000)
+//2.변수 2개가 같은 경우 1000 + (같은 눈) * 100(1,1,3 = 1100)
+//3.모든 변수가 다른 경우 max * 100 (1,2,3, = 300)
