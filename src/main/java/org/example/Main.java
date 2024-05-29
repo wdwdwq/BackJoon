6101dcff52ae3eb861327034d0a10123a0f67cfe
wdwdwq <qkenr7278@gmail.com>
2024-05-29 16:19:42 +0900

    BackJoon15552BufferedReader

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,28 @@
 package org.example;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String [] args) throws IOException {
 
-        int n = in.nextInt();
-        in.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for(int k = 0; k < i; k++){
-                System.out.print("*");
-            }
-            System.out.println();
+        int N = Integer.parseInt(br.readLine()); //N은 입력 받을 줄의 개수
+
+        StringTokenizer st ;
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
         }
+        br.close();
+
+        bw.flush(); // 한 번에 값을 출력할 때 사용
+        bw.close();
     }
 }
 
-
-
+// 별 찍기 반대로
