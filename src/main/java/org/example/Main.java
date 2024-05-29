c48d83ecc58fb52f46a81b5a8105f7d7983a7fb0
wdwdwq <qkenr7278@gmail.com>
2024-05-29 12:53:13 +0900

    BackJoon10950

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,20 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
 
+        int c = in.nextInt();
+        int arr[] = new int[c];
+
+        for(int i = 0; i < c; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            arr[i] = a + b;
+        }
         in.close();
 
-        for(int i = 1; i<10;i++){
-            System.out.println(a+" * "+i+" = "+(a*i));
+        for(int k : arr){
+            System.out.println(k);
         }
     }
 }
-
+// 배열 arr 안에 각 쌍 합을 계산 값이 저장되고 정수를 입력한만큼 저장된다
