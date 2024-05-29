548771759806bf3675c13317cf1bd4f709a06d25
wdwdwq <qkenr7278@gmail.com>
2024-05-29 11:48:52 +0900

    BackJoon 2480

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,65 @@
 package org.example;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+        // 만약 모든 변수가 다른 경우
+        if (a != b && b != c && a != c) {
+            int max;
+            // 만약 a > b 라면
+            if (a > b) {
+                // c > a > b 라면
+                if (c > a) {
+                    max = c;
+                }
+                // a > (b, c)
+                else {
+                    max = a;
+                }
+            }
+            // b > a 라면
+            else {
+                // c > b > a 라면
+                if (c > b) {
+                    max = c;
+                }
+                // b > (a, c)
+                else {
+                    max = b;
+                }
+            }
+            System.out.println(max * 100);
+        }
+        // 적어도 한 쌍 이상의 서로 같은 변수가 존재할 경우
+        else {
+            // 3개의 변수가 모두 같은 경우
+            if (a == b && a == c) {
+                System.out.println(10000 + a * 1000);
+            } else {
+                // a가 b혹은 c랑만 같은 경우
+                if (a == b || a == c) {
+                    System.out.println(1000 + a * 100);
+                }
+                // b가 c랑 같은 경우
+                else {
+                    System.out.println(1000 + b * 100);
+                }
+            }
+        }
     }
 }
+
+//1. 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+//2. 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+//3. 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
+
+//3개의 변수가 모두 다른, 아닌지 판단
+//
