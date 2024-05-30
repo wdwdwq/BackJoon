b0454eb4c7a3041bfa9e66e6c9ef1d633a1ed008
wdwdwq <qkenr7278@gmail.com>
2024-05-30 11:42:55 +0900

    BJ10951

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,19 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args)  {
 
-        StringBuilder sb = new StringBuilder();
+        Scanner in = new Scanner(System.in);
 
-        while(true){
-            String str = br.readLine();
+        while (in.hasNextInt()){
 
-            int A = str.charAt(0) - 48 ; // charAt() 문자로 반환 -48(-0) 사용 문자의 아스키 코드 값을 반환함
-            int B = str.charAt(2) - 48;
-
-            if(A == 0 && B == 0){
-                break;
-            }
-            sb.append((A+B)).append('\n');
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a+b);
         }
-        System.out.println(sb);
+        in.close();
     }
 }
 
-// 문제는 A+B를 출력하고 0, 0을 누르면 프로그램을 종료
-// Exit 입력으로 프로그램 탈출
-//
