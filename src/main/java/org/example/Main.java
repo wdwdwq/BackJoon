b421e73a17979960740d9aca1ea93fe84d997073
wdwdwq <qkenr7278@gmail.com>
2024-05-30 10:18:24 +0900

    BJ10952BufferedReader

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,30 @@
 package org.example;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
-            String input = in.nextLine();
+        StringBuilder sb = new StringBuilder();
 
-            if (input.equals("Exit")) {
-                in.close();
-                System.exit(0);
+        while(true){
+            String str = br.readLine();
+
+            int A = str.charAt(0) - 48 ; // charAt() 문자로 반환 -48(-0) 사용 문자의 아스키 코드 값을 반환함
+            int B = str.charAt(2) - 48;
+
+            if(A == 0 && B == 0){
+                break;
             }
-            String[] parts = input.split(" ");
-
-            int A = Integer.parseInt(parts[0]);
-            int B = Integer.parseInt(parts[1]);
-            System.out.println(A + B);
+            sb.append((A+B)).append('\n');
         }
-
+        System.out.println(sb);
     }
 }
 
 // 문제는 A+B를 출력하고 0, 0을 누르면 프로그램을 종료
 // Exit 입력으로 프로그램 탈출
+//
