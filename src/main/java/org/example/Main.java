fbcc08cbba886cc6d6a3358b78ba1affbe43a52e
wdwdwq <qkenr7278@gmail.com>
2024-05-30 09:48:06 +0900

    BJ10952Exit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,18 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        while (true){
+        while (true) {
+            String input = in.nextLine();
 
-            int A = in.nextInt();
-            int B = in.nextInt();
-
-            if(A == 0 && B == 0){
+            if (input.equals("Exit")) {
                 in.close();
-                break;
+                System.exit(0);
             }
-            System.out.println(A+B);
+            String[] parts = input.split(" ");
+
+            int A = Integer.parseInt(parts[0]);
+            int B = Integer.parseInt(parts[1]);
+            System.out.println(A + B);
         }
 
     }
 }
+
 // 문제는 A+B를 출력하고 0, 0을 누르면 프로그램을 종료
+// Exit 입력으로 프로그램 탈출
