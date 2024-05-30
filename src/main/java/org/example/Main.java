85a56e7be0051f1b84a2fd0f0b576ab600a3ef67
wdwdwq <qkenr7278@gmail.com>
2024-05-30 12:51:52 +0900

    BJ10951(2)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,15 +12,16 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         String str;
+        int N = Integer.parseInt(br.readLine());
 
 
-        while( (str=br.readLine()) != null ){
+        for(int i = 0; i < N; i++ ){
+            str = br.readLine();
 
             st = new StringTokenizer(str," ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             sb.append(a+b).append("\n");
-            System.out.println(a+b);
 
         }
         System.out.print(sb);
@@ -28,3 +29,4 @@ public class Main {
     }
 }
 
+// 특정 갯수만큼 결과를 한 번에 저장해 출력
